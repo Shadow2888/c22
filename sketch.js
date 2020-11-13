6783afa8f9f8d8627c60e7d29642b84c0026ab60
Shadow2888 <72982446+Shadow2888@users.noreply.github.com>
2020-11-13 20:09:33 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.7, isStatic:true});
 	World.add(world, packageBody);
 	
 
@@ -55,7 +55,7 @@ function draw() {
 }
 
 function keyPressed() {
- if (keydown("space")) {
+ if (keyDown("space")) {
     
     Matter.Body.setStatic(packageBody,false);
   }
